1fce6cadcf01d0c7d626b4ac94d6c0a1f124e162
Daniel <detz582@aucklanduni.ac.nz>
2020-06-09 11:44:22 +1200

    Create Company Class Update the ListView to match the book class example

---
+++ app/src/androidTest/java/Company.java
@@ -0,0 +1,41 @@
+public class Company {
+    private String name;
+    private String country;
+    private String id;
+    private int popularity;
+    private String description;
+    private int logoAddress;
+
+    public Company( String name, String id, String country, int logoAddress, int popularity, String description){
+        this.id = id;
+        this.country = country;
+        this.name = name;
+        this.logoAddress = logoAddress;
+        this.popularity = popularity;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry(){
+        return country;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public int getPopularity(){
+        return popularity;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getLogo(){
+        return logoAddress;
+    }
+}
