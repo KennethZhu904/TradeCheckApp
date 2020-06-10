35378784d915769baf426a29a2813542b1f3b87a
Kenneth Zhu <kzhu904@aucklanduni.ac.nz>
2020-06-10 20:20:48 +1200

    Create CompanyProvider.java

---
+++ app/src/main/java/com/example/tradecheck/CompanyProvider.java
@@ -0,0 +1,24 @@
+package com.example.tradecheck;
+
+public class CompanyProvider {
+
+
+    static String[] companyNames= {"Microsoft, PWC, Spark NZ, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
+
+    static String[] ids= {"001","002","003","004","005","006","007","008","009","010","011"};
+
+    static String[] countries={"NZ", "Australia", "United States", "Singapore" };
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
