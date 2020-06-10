6055ad491926d18ac66440521b347ee170cb98a1
Kenneth Zhu <kzhu904@aucklanduni.ac.nz>
2020-06-10 23:24:40 +1200

    Added in One card view and remodify the pictures of flags

--- app/src/main/java/com/example/tradecheck/CompanyProvider.java
+++ app/src/main/java/com/example/tradecheck/CompanyProvider.java
@@ -4,10 +4,9 @@ public class CompanyProvider {
 
 
     static String[] companyNames= {"Microsoft, PWC, Spark NZ, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
-
-    static String[] ids= {"001","002","003","004","005","006","007","008","009","010","011"};
-
-    static String[] countries={"NZ", "Australia", "United States", "Singapore" };
+    static String[] ids= {"001","002","003","004","005","006","007"};
+    static String[] countries={ "Australia","NZ", "Singapore","United States"};
+    static String[] pictures={"AU","NZ","SG","US"};
 
 
 
