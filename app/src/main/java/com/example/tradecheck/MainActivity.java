b6e0df6d8ddaec65f96754440bfbdb1543ade650
Daniel <detz582@aucklanduni.ac.nz>
2020-06-12 10:50:49 +1200

    Update Code Structure and add slider library
    - Add Company Detail Activity
    - Following online freeware and tutorial add a card and slider templete

--- app/src/main/java/com/example/tradecheck/MainActivity.java
+++ app/src/main/java/com/example/tradecheck/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tradecheck;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
