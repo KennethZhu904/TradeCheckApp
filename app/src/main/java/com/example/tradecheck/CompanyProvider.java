5a30c0bea279f7aa4b4463cb65cc4da82a046825
Daniel <detz582@aucklanduni.ac.nz>
2020-06-11 22:53:45 +1200

    Initialise a branch to create the company details layout/slider and dataset

--- app/src/main/java/com/example/tradecheck/CompanyProvider.java
+++ app/src/main/java/com/example/tradecheck/CompanyProvider.java
@@ -3,10 +3,10 @@ package com.example.tradecheck;
 public class CompanyProvider {
 
 
-    static String[] companyNames= {"Microsoft, PWC, Spark NZ, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
+    static String[] companyNames= {"Microsoft, PWC, Spark nz, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
     static String[] ids= {"001","002","003","004","005","006","007"};
-    static String[] countries={ "Australia","NZ", "Singapore","United States"};
-    static String[] pictures={"AU","NZ","SG","US"};
+    static String[] countries={ "Australia","nz", "Singapore","United States"};
+    static String[] pictures={"AU","nz","sg","us"};
 
 
 
