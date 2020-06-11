4ba26ebf819b0c1e735379caa7753d98b088a77e
Kenneth Zhu <kzhu904@aucklanduni.ac.nz>
2020-06-11 23:08:55 +1200

    Update CompanyProvider.java

--- app/src/main/java/com/example/tradecheck/CompanyProvider.java
+++ app/src/main/java/com/example/tradecheck/CompanyProvider.java
@@ -1,23 +1,33 @@
 package com.example.tradecheck;
 
+import java.util.ArrayList;
+
 public class CompanyProvider {
 
 
-    static String[] companyNames= {"Microsoft, PWC, Spark NZ, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
-    static String[] ids= {"001","002","003","004","005","006","007"};
+   // static String[] companyNames= {"Microsoft, PWC, Spark NZ, Fonterra, HoneyWell, Qantas,Singapore Airlines "};
+    static String[] ids= {"001","002","003","004"};
     static String[] countries={ "Australia","NZ", "Singapore","United States"};
-    static String[] pictures={"AU","NZ","SG","US"};
+    static int[] pictures={R.drawable.AU,R.drawable.NZ,R.drawable.SG,R.drawable.US};
 
 
+    public static String[] getCountries() {
+        return countries;
+    }
+
+    public static ArrayList<Countries> generateData() {
+        ArrayList<Countries> listOfCountries = new ArrayList<Countries>();
 
 
+        for (int i = 0; i < 4; i++) {
 
-
-
-
-
-
-
-
+            String id = ids[i];
+            String countriesname = countries[i];
+            int flags = pictures[i];
+            Countries aCountry = new Countries(countriesname, flags, id);
+            listOfCountries.add(aCountry);
+        }
+        return listOfCountries;
+    }
 
 }
