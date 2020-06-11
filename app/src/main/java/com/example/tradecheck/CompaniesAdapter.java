9babfcf3bf0015a7483d1fc53264cd7dcb61de57
Kenneth Zhu <kzhu904@aucklanduni.ac.nz>
2020-06-12 00:42:02 +1200

    Update CompaniesAdapter.java

--- app/src/main/java/com/example/tradecheck/CompaniesAdapter.java
+++ app/src/main/java/com/example/tradecheck/CompaniesAdapter.java
@@ -18,10 +18,56 @@ import java.util.List;
 
 public class CompaniesAdapter extends ArrayAdapter<Countries> {
 
+    private static class ViewHolder{
+
+        public ImageView countryflag;
+        public TextView  countrytext;
+
+    }
+
+
     public CompaniesAdapter(Context context, ArrayList<Countries> aCountry){
 
         super(context, 0, aCountry);
 
     }
+
+    //Translates a particular 'Country' given a position
+    //into a relevant row ithin an AdapterView
+
+    //get the data item for this position
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        final Countries country = getItem(position);
+
+        //Check if an existing view is being reused, otherwise inflate the view
+
+        ViewHolder viewHolder;
+
+        if (convertView == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.category_layout_list,parent,false);
+            viewHolder.countryflag = (ImageView)convertView.findViewById(R.id.countryflag);
+            viewHolder.countrytext = (TextView)convertView.findViewById(R.id.countrytext);
+            convertView.setTag(viewHolder);
+        } else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.countrytext.setText(country.getName());
+        int imageID=country.getFlagFileName();
+        viewHolder.countryflag.setImageResource(imageID);
+
+
+        return convertView;
+    }
+
+
+
+
+
+
 
 }
