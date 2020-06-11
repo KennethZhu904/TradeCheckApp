16690f3d4d9da840340dd026eb30a3d1d6b03cf0
Kenneth Zhu <kzhu904@aucklanduni.ac.nz>
2020-06-11 23:10:50 +1200

    Update MainActivity.java

--- app/src/main/java/com/example/tradecheck/MainActivity.java
+++ app/src/main/java/com/example/tradecheck/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.tradecheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+          String[] countries= CompanyProvider.getCountries();
+        List<String> countriesNames=new ArrayList(Arrays.asList(countries));
+
+        ArrayAdapter<String> countriesAdapter=new ArrayAdapter<String>(this,R.layout.category_layout_list,countriesNames);
+        ListView listView= findViewById(R.id.tradeview);
+
     }
 }
