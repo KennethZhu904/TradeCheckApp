a8e895a9941c6823e3dcb70ca8542a2e797d3cc3
Daniel <detz582@aucklanduni.ac.nz>
2020-06-13 17:36:09 +1200

    Implement a draft detail view layout

--- app/src/main/java/com/example/tradecheck/MainActivity.java
+++ app/src/main/java/com/example/tradecheck/MainActivity.java
@@ -22,46 +22,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company__details);
+        setContentView(R.layout.activity_main);
 
         //Declare the ListView and the Adapter
-//        ListView tradeview;
-//        CompaniesAdapter companyAdapter;
-//        ArrayList<Countries> aCompany;
-//
-//        tradeview = (ListView) findViewById(R.id.tradeview);
-//        ArrayList<Countries> aBooks = new ArrayList<Countries>();
-//
-//
-//        aBooks = CompanyProvider.generateData();
-//        companyAdapter = new CompaniesAdapter(this, aBooks);
-//
-//        //Set that adapter to the listview
-//        tradeview.setAdapter(companyAdapter);
-//
-//        LinearLayoutManager lm = new LinearLayoutManager(this);
+        ListView tradeview;
+        CompaniesAdapter companyAdapter;
+        ArrayList<Countries> aCompany;
 
-        SliderView sliderView = findViewById(R.id.imageSlider);
-
-        StockSliderAdapter adapter = new StockSliderAdapter(this);
+        tradeview = (ListView) findViewById(R.id.tradeview);
+        ArrayList<Countries> aBooks = new ArrayList<Countries>();
 
 
-        adapter.addItem(new SliderItem("Day", "https://media.wired.com/photos/5b8999943667562d3024c321/master/w_2560%2Cc_limit/trash2-01.jpg" ));
-        adapter.addItem(new SliderItem("Month", "https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg"));
-        adapter.addItem(new SliderItem("Year", "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2013/12/19/1387462292398/c447b512-ea0b-423e-b5f4-7f66b0db44bb-620x372.jpeg?width=300&quality=45&auto=format&fit=max&dpr=2&s=b1875610cc7ed39c7865f42563ba75cd"));
-        sliderView.setSliderAdapter(adapter);
+        aBooks = CompanyProvider.generateData();
+        companyAdapter = new CompaniesAdapter(this, aBooks);
 
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
-        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-        sliderView.setIndicatorUnselectedColor(Color.GRAY);
-        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
-        sliderView.startAutoCycle();
-
+        //Set that adapter to the listview
+        tradeview.setAdapter(companyAdapter);
 
         LinearLayoutManager lm = new LinearLayoutManager(this);
-        }
 
 
     }
+}
